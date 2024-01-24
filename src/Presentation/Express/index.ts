cbb98cb350614da8195870998b034a4a75e19185
yousakamori <yousakamori@gmail.com>
2024-01-25 07:47:52 +0900

    added presentation

--- src/Presentation/Express/index.ts
+++ src/Presentation/Express/index.ts
@@ -1,3 +1,10 @@
+import {
+  RegisterBookApplicationService,
+  RegisterBookCommand,
+} from "Application/Book/RegisterBookApplicationService/RegisterBookApplicationService";
+import { PrismaBookRepository } from "Infrastructure/Prisma/Book/PrismaBookRepository";
+import { PrismaClientManager } from "Infrastructure/Prisma/PrismaClientManager";
+import { PrismaTransactionManager } from "Infrastructure/Prisma/PrismaTransactionManager";
 import express from "express";
 
 const app = express();
@@ -9,4 +16,35 @@ app.get("/", (_, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listen on port ${port}`);
+});
+
+app.use(express.json());
+app.post("/book", async (req, res) => {
+  try {
+    const requestBody = req.body as {
+      isbn: string;
+      title: string;
+      priceAmount: number;
+    };
+
+    const clientManager = new PrismaClientManager();
+    const transactionManager = new PrismaTransactionManager(clientManager);
+    const bookRepository = new PrismaBookRepository(clientManager);
+
+    const registerBookApplicationService = new RegisterBookApplicationService(
+      bookRepository,
+      transactionManager
+    );
+
+    const registerCommand: RegisterBookCommand = requestBody;
+    await registerBookApplicationService.execute(registerCommand);
+
+    res.status(200).json({ message: "success" });
+  } catch (err) {
+    res.status(500).json({ message: (err as Error).message });
+  }
+});
+
+app.post("/book", async (_req, res) => {
+  res.status(200).json({ message: true });
 });
